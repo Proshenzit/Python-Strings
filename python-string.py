53ad0d3ffa7a5a8922c246978abec4d112a4247e
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-04-07 04:03:48 -0500

    Add files via upload

---
+++ python-string.py
@@ -0,0 +1,49 @@
+string1 = "proshenzit roy"
+print(string1)
+
+string2 = 'Redela roy'
+print(string2)
+
+string2 = 'Redela roy'
+print(string2[1])
+
+string2 = 'Redela roy'
+print(string2[:4])
+
+str1 ="hellow are you "
+str2 ="i love you"
+str3 ="hellow are you "
+print(str1==str2)
+print(str1==str3)
+
+
+name= "proshenzit roy"
+name1=" rodela "
+result = name + name1
+print(result)
+
+
+greet = "hello"
+for letter   in greet:
+  print(letter)
+
+
+str1 ="hellow are you "
+
+print(len(str1))
+
+print('a' in  'progam')
+
+print('at' in  'progam')
+
+
+example = "he said ,\"what,s there \""
+
+print(example)
+
+name ="cathy"
+country = "usa"
+print(f'{name} is  from {country }')
+
+
+
